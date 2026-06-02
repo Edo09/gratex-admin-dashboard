ddf858b646b92ed48d778eb71afcdfcff02ab8be
edo09 <omareogm09@gmail.com>
2026-06-02 00:10:19 -0400

    fix: make VITE_API_URL a required property in ImportMetaEnv interface

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL?: string;
+  readonly VITE_API_URL: string;
   readonly VITE_SHOW_DEBUG_PAYLOAD?: string;
 }
 
